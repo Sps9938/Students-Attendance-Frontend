322864ad0a25d498ddd6340cc709ca3c57465307
satya prakash <satya200prakash@gmail.com>
2025-04-15 17:14:31 +0530

    fist commit

---
+++ student_attendance/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className='text-3xl text-white bg-red-400'>Student_Attendance</h1>
+    </>
+  )
+}
+
+export default App
