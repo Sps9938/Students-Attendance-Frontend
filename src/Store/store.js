20bc7100c3360829ccc0b29813e1380ca1a57925
satya prakash <satya200prakash@gmail.com>
2025-07-09 21:47:38 +0530

    set correcly file path-> issue deploy time

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authSlice from './authSlice';
-// import classSlice from './classSlice';
+import authSlice from "../authSlice";
+
 
 const store = configureStore( {
     reducer: {
         auth: authSlice,
-        // classes: classSlice,
+       
    
     }
 })
